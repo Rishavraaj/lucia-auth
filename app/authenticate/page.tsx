b1dc9e71a61665df77f2015d84916a8c3f03b409
Rishavraaj <rishavrajpatna7@gmail.com>
2024-12-17 16:40:16 +0530

    feat: added google auth button

--- app/authenticate/page.tsx
+++ app/authenticate/page.tsx
@@ -4,6 +4,7 @@ import SignUpForm from "./signup-form";
 import { TabSwitcher } from "@/components/tab-switcher";
 import React from "react";
 import { redirect } from "next/navigation";
+import GoogleSignInButton from "@/components/google-oauth-button";
 
 export default async function AuthenticatePage() {
   const user = await getUser();
@@ -12,8 +13,9 @@ export default async function AuthenticatePage() {
   }
   return (
     <div className="relative flex h-screen w-full bg-background">
-      <div className="max-w-3xl absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="max-w-3xl flex flex-col gap-4 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
         <TabSwitcher signUpTab={<SignUpForm />} signInTab={<SignInForm />} />
+        <GoogleSignInButton className="w-full" />
       </div>
     </div>
   );
