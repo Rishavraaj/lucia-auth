50faea7c65c91620054cc85d1a31cc1fdcda1720
Rishavraaj <rishavrajpatna7@gmail.com>
2024-12-17 14:27:26 +0530

    feat: added logic to getUser

--- lib/lucia.ts
+++ lib/lucia.ts
@@ -1,6 +1,7 @@
 import { Lucia } from "lucia";
 import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
 import { db } from "./prisma";
+import { cookies } from "next/headers";
 
 const adapter = new PrismaAdapter(db.session, db.user);
 
@@ -14,3 +15,47 @@ export const lucia = new Lucia(adapter, {
     },
   },
 });
+
+export const getUser = async () => {
+  const sessionId = (await cookies()).get(lucia.sessionCookieName)?.value;
+
+  if (!sessionId) {
+    return null;
+  }
+
+  const { user, session } = await lucia.validateSession(sessionId);
+
+  try {
+    if (session && session.fresh) {
+      //refresh the session cookie
+      const sessionCookie = await lucia.createSessionCookie(session.id);
+      (await cookies()).set(
+        lucia.sessionCookieName,
+        sessionCookie.value,
+        sessionCookie.attributes
+      );
+    }
+    if (!session) {
+      //delete the session cookie
+      const blankSessionCookie = await lucia.createBlankSessionCookie();
+      (await cookies()).set(
+        lucia.sessionCookieName,
+        blankSessionCookie.value,
+        blankSessionCookie.attributes
+      );
+    }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+  const dbUser = await db.user.findUnique({
+    where: {
+      id: user?.id,
+    },
+    select: {
+      name: true,
+      email: true,
+    },
+  });
+  return dbUser;
+};
