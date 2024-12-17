27aa88e53cba84f8c9dd37f5ec0e2e7dcbbbebef
Rishavraaj <rishavrajpatna7@gmail.com>
2024-12-17 13:28:19 +0530

    feat; installed oslo package

--- app/authenticate/auth.action.ts
+++ app/authenticate/auth.action.ts
@@ -1,8 +1,24 @@
 "use server";
 
+import { db } from "@/lib/prisma";
 import { signUpSchema } from "./signup-form";
 import { z } from "zod";
 
 export const signUp = async (data: z.infer<typeof signUpSchema>) => {
-  console.log(data);
+  try {
+    const existingUser = await db.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (existingUser) {
+      return {
+        error: "User already exists",
+        success: false,
+      };
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
